2860f523ceade17fd04276bbea9b49075df06b2d
DucH1910 <84374534+DucH1910@users.noreply.github.com>
2022-05-13 13:49:35 +0930

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "QuickSort.h"
+#include "BubbleSort.h"
+#include "RecursiveBinarySearch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+    vector<int> list;
+    while (!cin.eof()) {
+        string temp;
+        cin >> temp;
+        if (temp == "")
+            break
+        list.push_back(stoi(temp));
+    }
+    QuickSort quick;
+    vector<int> sortedList = quick.sort(list);
+    for(int i=0;i<list.size();i++)
+        cout<<sortedList.at(i)<<" ";
+    cout<<endl;
+    RecursiveBinarySearch search;
+    cout<<search.search(sortedList,3)<<endl;
+    return 0;
+}
